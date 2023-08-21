703b82757332f96e34fdea6596b6274ab3784f03
Thiago São Pedro Oliveira <thiagosaopedroo07@gmail.com>
2023-08-21 18:19:13 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+    //if(html.classList.contains('light')){
+       // html.classList.remove('light')
+    //} else{
+       // html.classList.add('light')
+    //}
+
+
+        const img = document.querySelector("#profile img");
+    
+        if (html.classList.contains('light')) {
+            img.setAttribute('src', './assets/avatar-light.png'); // Correção aqui
+        } else {
+            img.setAttribute('src', './assets/avatar.png'); // Correção aqui
+        }
+    }
+    
